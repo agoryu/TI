0ae05bb3679493db4a1a71ac04d840c5874ad840
Gaëtan DEFLANDRE <gaetan.deflandre@gmail.com>
2015-04-16 23:55:16 +0200

    nouvelle boucle

--- tp12/src/DCT/DCT2D.java
+++ tp12/src/DCT/DCT2D.java
@@ -1,7 +1,8 @@
 package DCT;
 
-
 import ij.process.FloatProcessor; // Pour classe Float Processor
+
+import java.awt.Rectangle;
 
 abstract public class DCT2D {
 
@@ -16,34 +17,38 @@ abstract public class DCT2D {
 	 */
 	public static void forwardDCT(FloatProcessor fp) {
 
-		final int W = fp.getWidth();
-		final int H = fp.getHeight();
+		final Rectangle roi = fp.getRoi();
+		final int newW = roi.width;
+		final int newH = roi.height;
+		final int beginX = roi.x;
+		final int beginY = roi.y;
 
+		
 		// Traiter les lignes
-		for (int y = 0; y < H; y++) {
+		for (int y = 0; y < newH; y++) {
 
-			double[] row_F = DCT1D.forwardDCT(fp.getLine(0, y, W - 1, y));
+			double[] row_F = DCT1D.forwardDCT(fp.getLine(0, y+beginY, newW - 1, y+beginY));
 
-			for (int x = 0; x < W; x++) {
-				fp.putPixelValue(x, y, row_F[x]);
+			for (int x = 0; x < newW; x++) {
+				fp.putPixelValue(x+beginX, y+beginY, row_F[x]);
 			}
 		}
 
 		// Traiter les colonnes de l'image résultant du traitement des
 		// lignes
-		for (int x = 0; x < W; x++) {
+		for (int x = 0; x < newW; x++) {
 
-			double[] col_f = new double[W];
-			double[] col_F = new double[W];
+			double[] col_f = new double[newW];
+			double[] col_F = new double[newW];
 
-			for (int y = 0; y < H; y++) {
-				col_f[y] = fp.getPixelValue(x, y);
+			for (int y = 0; y < newH; y++) {
+				col_f[y] = fp.getPixelValue(beginX+x, beginY+y);
 			}
 
 			col_F = DCT1D.forwardDCT(col_f);
 
-			for (int y = 0; y < W; y++) {
-				fp.putPixelValue(x, y, col_F[y]);
+			for (int y = 0; y < newW; y++) {
+				fp.putPixelValue(beginX+x, beginY+y, col_F[y]);
 			}
 		}
 	}
@@ -57,10 +62,39 @@ abstract public class DCT2D {
 	 */
 	public static void inverseDCT(FloatProcessor fp) {
 
-		// Traiter les lignes
-		/* � compl�ter */
+		final Rectangle roi = fp.getRoi();
+		final int newW = roi.width;
+		final int newH = roi.height;
+		final int beginX = roi.x;
+		final int beginY = roi.y;
 
-		// Traiter les colonnes de l'image r�sultant du traitement des lignes
-		/* � compl�ter */
+		
+		// Traiter les lignes
+		for (int y = 0; y < newH; y++) {
+
+			double[] row_F = DCT1D.inverseDCT(fp.getLine(0, y+beginY, newW - 1, y+beginY));
+
+			for (int x = 0; x < newW; x++) {
+				fp.putPixelValue(x+beginX, y+beginY, row_F[x]);
+			}
+		}
+
+		// Traiter les colonnes de l'image résultant du traitement des
+		// lignes
+		for (int x = 0; x < newW; x++) {
+
+			double[] col_f = new double[newW];
+			double[] col_F = new double[newW];
+
+			for (int y = 0; y < newH; y++) {
+				col_f[y] = fp.getPixelValue(beginX+x, beginY+y);
+			}
+
+			col_F = DCT1D.inverseDCT(col_f);
+
+			for (int y = 0; y < newW; y++) {
+				fp.putPixelValue(beginX+x, beginY+y, col_F[y]);
+			}
+		}
 	}
 }
--- tp12/src/DCT_plugin.java
+++ tp12/src/DCT_plugin.java
@@ -1,5 +1,7 @@
 import ij.ImagePlus;
 import ij.plugin.filter.PlugInFilter;
+import ij.process.Blitter;
+import ij.process.ByteProcessor;
 import ij.process.FloatProcessor;
 import ij.process.ImageProcessor;
 import DCT.DCT2D;
@@ -27,6 +29,20 @@ public class DCT_plugin implements PlugInFilter {
 	 */
 	private int height;
 
+	final static int BLOCK_SIZE = 8;
+
+	public final static int[][] QY = { { 16, 12, 14, 14, 18, 24, 49, 72 },
+			{ 11, 12, 13, 17, 22, 35, 64, 92 },
+			{ 10, 14, 16, 22, 37, 55, 78, 95 },
+			{ 16, 19, 24, 29, 56, 64, 87, 98 },
+			{ 24, 26, 40, 51, 68, 81, 103, 112 },
+			{ 40, 58, 57, 87, 109, 104, 121, 100 },
+			{ 51, 60, 69, 80, 103, 113, 120, 103 },
+			{ 61, 55, 56, 62, 77, 92, 101, 99 } };
+
+	private static ByteProcessor bpQuantification = new ByteProcessor(
+			BLOCK_SIZE, BLOCK_SIZE);
+
 	// METHODS //
 
 	/**
@@ -43,13 +59,76 @@ public class DCT_plugin implements PlugInFilter {
 		// Lecture des dimensions de la fenêtre
 		width = imp.getWidth();
 		height = imp.getHeight();
+		bpQuantification.setIntArray(QY);
 
+		// Compression
 		FloatProcessor fp = ip.convertToFloatProcessor();
 		fp.add(-128);
-		DCT2D.forwardDCT(fp);
+
+		for (int y = 0; y < height; y += BLOCK_SIZE) {
+			for (int x = 0; x < width; x += BLOCK_SIZE) {
+
+				int roiWidth = BLOCK_SIZE;
+				int roiHeight = BLOCK_SIZE;
+
+				if (x + BLOCK_SIZE >= width) {
+					roiWidth = width - x;
+				}
+				if (y + BLOCK_SIZE >= height) {
+					roiHeight = height - y;
+				}
+
+				fp.setRoi(x, y, roiWidth, roiHeight);
+
+				DCT2D.forwardDCT(fp);
+			}
+		}
+		
+		fp.setRoi(0, 0, width, height);
+
+		/* DCT2D.forwardDCT(fp); */
+		fp.copyBits(bpQuantification, 0, 0, Blitter.DIVIDE);
+
+		// arrondi
+		for (int y = 0; y < width; y++) {
+			for (int x = 0; x < height; x++) {
+				int value = Math.round(fp.getf(x, y));
+				fp.putPixelValue(x, y, value);
+			}
+		}
 
 		ImagePlus frame = new ImagePlus("DCT", fp);
 		frame.show();
+
+		// Decompression
+		FloatProcessor fp2 = new FloatProcessor(fp.getFloatArray());
+		fp2.copyBits(bpQuantification, 0, 0, Blitter.MULTIPLY);
+
+		for (int y = 0; y < height; y += BLOCK_SIZE) {
+			for (int x = 0; x < width; x += BLOCK_SIZE) {
+
+				int roiWidth = BLOCK_SIZE;
+				int roiHeight = BLOCK_SIZE;
+
+				if (x + BLOCK_SIZE >= width) {
+					roiWidth = width - x;
+				}
+				if (y + BLOCK_SIZE >= height) {
+					roiHeight = height - y;
+				}
+
+				fp2.setRoi(x, y, roiWidth, roiHeight);
+				DCT2D.inverseDCT(fp2);
+			}
+		}
+		
+		fp2.setRoi(x, y, roiWidth, roiHeight);
+
+		/* DCT2D.inverseDCT(fp2); */
+		fp2.add(128);
+
+		ImagePlus frame2 = new ImagePlus("DCT2", fp2);
+		frame2.show();
 	}
 
 }
